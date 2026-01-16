70bcc10dd987742aef34de7167bb6a9e50a19c05
Jose Luis Pineda Barrera <123666863+Jlpineda12@users.noreply.github.com>
2026-01-15 21:22:43 -0500

    Delete portfolio directory

--- portfolio/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: './', // Use relative base for safer deployment if repo name is unknown
-  plugins: [react()],
-})
